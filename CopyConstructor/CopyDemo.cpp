5f73c0c826e76d3c14fe27123ecb2f1c0aedcef1
teja5kale <tejas.kale@cctech.co.in>
2024-10-23 23:59:55 +0530

    Updated file

--- CopyConstructor/CopyDemo.cpp
+++ CopyConstructor/CopyDemo.cpp
@@ -1,14 +1,11 @@
 #include "CopyDemo.h"
 
-// Default constructor
 CopyDemo::CopyDemo(int num, const std::string& str)
     : number(num), text(str) {}
 
-// Copy constructor
 CopyDemo::CopyDemo(const CopyDemo& other)
     : number(other.number), text(other.text) {}
 
-// Method to display member values
 void CopyDemo::display() const {
     std::cout << "Number: " << number << ", Text: " << text << std::endl;
 }
