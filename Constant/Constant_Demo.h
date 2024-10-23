4182f2c049b3b9af0a11642c0c549464a34d4bb6
teja5kale <tejas.kale@cctech.co.in>
2024-10-24 00:01:30 +0530

    Updated file

--- Constant/Constant_Demo.h
+++ Constant/Constant_Demo.h
@@ -3,22 +3,17 @@
 
 class Constant_Demo {
 private:
-    const int constantValue; // Constant data member
+    const int constantValue;
     int variableValue;
 
 public:
-    // Constructor to initialize both values
     Constant_Demo(int cv, int vv);
 
-    // Constant member function (does not modify any class member)
     void showConstant() const;
 
-    // Non-constant member function (can modify non-constant members)
     void modifyVariable(int newValue);
 
-    // Getter for non-constant member
     int getVariable() const;
 
-    // Constant pointer function (pointer to constant object)
     void showWithConstantPointer(const Constant_Demo* const obj) const;
 };
