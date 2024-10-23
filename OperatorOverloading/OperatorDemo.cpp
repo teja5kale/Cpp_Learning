c1346e42b9eb052a1c9abc139ad31eefbbd9b48c
teja5kale <tejas.kale@cctech.co.in>
2024-10-24 00:01:07 +0530

    Updated file

--- OperatorOverloading/OperatorDemo.cpp
+++ OperatorOverloading/OperatorDemo.cpp
@@ -1,19 +1,15 @@
 #include "OperatorDemo.h"
 
-// Constructor to initialize vector components
 Vector::Vector(double x, double y) : x(x), y(y) {}
 
-// Overloaded addition operator
 Vector Vector::operator+(const Vector& other) const {
     return Vector(x + other.x, y + other.y);
 }
 
-// Overloaded subtraction operator
 Vector Vector::operator-(const Vector& other) const {
     return Vector(x - other.x, y - other.y);
 }
 
-// Overloaded output stream operator
 std::ostream& operator<<(std::ostream& os, const Vector& vec) {
     os << "(" << vec.x << ", " << vec.y << ")";
     return os;
--- OperatorOverloading/OperatorDemo.h
+++ OperatorOverloading/OperatorDemo.h
@@ -3,19 +3,16 @@
 
 class Vector {
 public:
-    // Constructor
+   
     Vector(double x = 0, double y = 0);
 
-    // Overloaded addition operator
     Vector operator+(const Vector& other) const;
 
-    // Overloaded subtraction operator
     Vector operator-(const Vector& other) const;
 
-    // Overloaded output stream operator
     friend std::ostream& operator<<(std::ostream& os, const Vector& vec);
 
 private:
-    double x; // x component
-    double y; // y component
+    double x; 
+    double y; 
 };
