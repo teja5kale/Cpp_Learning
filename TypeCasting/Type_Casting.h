b7b3c797e14f1d2cd666d5cd53d0d254a14b9231
teja5kale <tejas.kale@cctech.co.in>
2024-10-24 00:00:31 +0530

    Updated file

--- TypeCasting/Type_Casting.h
+++ TypeCasting/Type_Casting.h
@@ -1,25 +1,19 @@
-
-
 #pragma once
 #include <iostream>
 #include <string>
 
 class Type_casting {
 public:
-    // Function to demonstrate implicit and explicit typecasting with primitive types
+    
     void primitiveTypecasting();
 
-    // Function to demonstrate casting between base and derived classes
     void classTypecasting();
 
-    // Function to demonstrate reinterpret casting and potential risks
     void riskyTypecasting();
 
-    // Function to demonstrate const_cast
     void constTypecasting();
 };
 
-// Base and derived class for demonstrating class typecasting
 class Base {
 public:
     virtual void show() const;
